eae3320fbf3311638162acf37daaae75d8818641
Alexandre CARVUNIS <carvunis@lapthui7b.in2p3.fr>
2021-07-28 16:42:49 +0200

    include NP cov in FL

--- flavio/__init__.py
+++ flavio/__init__.py
@@ -10,4 +10,4 @@ from .config import config
 from . import citations
 from flavio.physics.eft import WilsonCoefficients
 from flavio.parameters import default_parameters
-from flavio.functions import sm_prediction, sm_uncertainty, np_uncertainty, sm_error_budget, np_prediction, sm_covariance, combine_measurements
+from flavio.functions import sm_prediction, sm_uncertainty, np_uncertainty, sm_error_budget, np_prediction, sm_covariance, combine_measurements, np_covariance
